d3c3124bea3e2b42308e7bbf15be0f5ba121b6d0
Ben Wheeler <wheeler.benjamin@gmail.com>
2015-03-28 21:54:02 -0400

    two pages, less stuff

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,12 +8,43 @@ $(window).scroll(function(e) {
 });
 
 $(document).ready(function() {
-Parse.initialize("VRWD786nEZSzo3qEIPiLtbvONegjGyXT5M8DJtHs", "Ym7pDr16KApRj6nV7vEsgOl3LF5VRTMOrgVzl5xR");
+  Parse.initialize("VRWD786nEZSzo3qEIPiLtbvONegjGyXT5M8DJtHs", "Ym7pDr16KApRj6nV7vEsgOl3LF5VRTMOrgVzl5xR");
+  
+  
+  $('#signup_form').on("submit", function(event) {
+    submitToParse();
+    event.preventDefault();
+  });
+  $('#signup_button').on("click", function(event) {
+    submitToParse();
+    event.preventDefault();
+  });
+  
+  
+  var submitToParse = function() {
+    var email_val = $('#signup_email').val();
+    var Signup = Parse.Object.extend("Signup");
+    var signup = new Signup();
+    signup.save({email: email_val}).then(function(object) {
+      signupSuccess();
+    });				 
+  };
+  var submitToParse = function() {
+    $("#form_before").fadeOut(1000, function() {
+      alert();
+      $("#myModal").toggle();
+    });
+    $("#form_after").fadeIn("fast", function() {
+      
+    });
+  };
 
-var TestObject = Parse.Object.extend("Signup");
-var testObject = new TestObject();
-//testObject.save({email: "bar"}).then(function(object) {
-//  alert("yay! it worked");
-//});
+$('#try').on('click', function() {
+   window.location.href="circuits.html";
+
+});
+
+//var mp = $('#youtube')[0];
+//mp.setVolume(0);
 
 });
